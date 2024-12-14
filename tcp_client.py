20a4c1127f14dcb537744cdeb3d1b93dc1b28d5e
jojojo8359 <31038772+jojojo8359@users.noreply.github.com>
2024-12-14 14:50:23 -0500

    Add mutual authentication

--- tcp_client.py
+++ tcp_client.py
@@ -1,17 +1,15 @@
-import socket
-import ssl
-import argparse
 from filenames import CA_CERT_FILE, CLIENT_CERT_FILE, CLIENT_KEY_FILE
 from ca import save_cert, save_private_key, build_csr, sign_csr, validate_cert
 from ecdh import ec_gen_private_key
-import sys
+import socket, ssl, argparse, sys, time
+from cryptography import x509
 
 # RECEIVER
 parser = argparse.ArgumentParser(prog="tcp_client", description="Receive files with encryption and authentication")
 parser.add_argument('host_address', type=str, help='IP Address of the server to connect to')
 parser.add_argument('host_port', type=int, help='Port number of the server to connect to')
 parser.add_argument('email', type=str, help='The user\'s email')
-parser.add_argument('server_email', type=str, help='The email of the user hosting the server')
+parser.add_argument('peer_email', type=str, help='The email of the user to connect to')
 args = parser.parse_args()
 
 
@@ -33,17 +31,19 @@ context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH, cafile=CA_CERT_FIL
 context.load_cert_chain(certfile=CLIENT_CERT_FILE, keyfile=CLIENT_KEY_FILE)
 
 with socket.create_connection((args.host_address, args.host_port)) as sock:
-    with context.wrap_socket(sock, server_side=False, server_hostname=args.server_email) as ssock:
-        print(ssock.version())
-        ssock.do_handshake()
-        # prepare to transfer
-        # receiver pub/priv keys should be created if not already
-        
-        # receiver sends sender: public key
-        # with open(args.key, 'r') as f:
-        #     keydata = f.read()
-        # ssock.send(keydata.encode())
-        # wait for ack
-        # receiver sends sender: certificate
-        # wait for auth ack
-        # authenticated
+    try:
+        print("Client: connection established to " + args.host_address + ":" + args.host_port)
+        with context.wrap_socket(sock, server_side=False, server_hostname=args.server_email) as ssock:
+            print("Client: connection upgraded to SSL")
+            print(ssock.version())
+            ssock.do_handshake(block=True)
+            print("Client: handshake done")
+            peer_cert = x509.load_der_x509_certificate(ssock.getpeercert(binary_form=True))
+            if validate_cert(peer_cert, args.peer_email):
+                print("Client: successfully validated server certificate!")
+            else:
+                print("Client: couldn't validate server certificate!")
+            time.sleep(1)
+    finally:
+        print("Client: connection closed")
+        sock.close()
--- tcp_server.py
+++ tcp_server.py
@@ -1,18 +1,15 @@
-import socket
-import ssl
-import argparse
 from filenames import CA_CERT_FILE, CLIENT_CERT_FILE, CLIENT_KEY_FILE
 from ca import save_cert, save_private_key, build_csr, sign_csr, validate_cert
 from ecdh import ec_gen_private_key
-import sys
-# from cryptography import x509
-# from cryptography.hazmat.backends import default_backend
+import socket, ssl, argparse, sys, time
+from cryptography import x509
 
 # SENDER
 parser = argparse.ArgumentParser(prog="tcp_server", description="Receive files with encryption and authentication")
 parser.add_argument('host_address', type=str, help='IP Address of the server to host')
 parser.add_argument('host_port', type=int, help='Port number of the server to host')
 parser.add_argument('email', type=str, help='The user\'s email')
+parser.add_argument('peer_email', type=str, help='The email of the user to connect to')
 args = parser.parse_args()
 
 
@@ -30,23 +27,27 @@ if not validate_cert(cert, args.email):
 save_cert(CLIENT_CERT_FILE, cert)
 
 
-# context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH, cafile='ca.cert.pem')
 context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 context.verify_mode = ssl.CERT_REQUIRED
 context.load_cert_chain(certfile=CLIENT_CERT_FILE, keyfile=CLIENT_KEY_FILE)
 context.load_verify_locations(cafile=CA_CERT_FILE)
 
-# with open(args.certificate, 'rb') as f:
-#     cert = x509.load_pem_x509_certificate(f.read(), default_backend())
-# print(cert.subject.rdns[0])
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
     sock.bind((args.host_address, args.host_port))
     sock.listen(5)
     with context.wrap_socket(sock, server_side=True) as ssock:
+        print("Server: waiting for connection")
         conn, addr = ssock.accept()
-        print(str(addr) + " connected")
-        # pubkey = conn.recv(2048).decode()
-        conn.do_handshake()
-        print(conn.getpeername())
-        # print(pubkey)
+        try:
+            print(str(addr) + " connected")
+            conn.do_handshake(block=True)
+            print("Server: handshake done")
+            peer_cert = x509.load_der_x509_certificate(conn.getpeercert(binary_form=True))
+            if validate_cert(peer_cert, args.peer_email):
+                print("Server: successfully validated client certificate!")
+            else:
+                print("Server: couldn't validate client certificate!")
+            time.sleep(1)
+        finally:
+            conn.close()
+            print("Server: connection closed")
